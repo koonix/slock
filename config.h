2e5d6f99717a9928583fc0339293a6d5b727f7f6
ehsan gn <ehsangn@protonmail.ch>
2021-06-03 12:17:18 +0430

    change colors and message

--- config.h
+++ config.h
@@ -4,21 +4,21 @@ static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INPUT] =  "#003300",   /* during input */
+	[FAILED] = "#330000",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
 /* time in seconds before the monitor shuts down */
-static const int monitortime = 5;
+static const int monitortime = 8;
 
 /* default message */
-static const char * message = "Suckless: Software that sucks less.";
+static const char * message = "Enter Password Please";
 
 /* text color */
-static const char * text_color = "#ffffff";
+static const char * text_color = "#aaaaaa";
 
 /* text size (must be a valid size) */
 static const char * font_name = "-uw-ttyp0-medium-r-normal--22-200-75-75-c-110-iso8859-15";
